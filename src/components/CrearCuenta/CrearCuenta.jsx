0d160306775ec2ab51baf60fe1836ac00c393707
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-25 18:49:40 -0300

    Creando formulario para crear cuenta

--- src/components/CrearCuenta/CrearCuenta.jsx
+++ src/components/CrearCuenta/CrearCuenta.jsx
@@ -1,3 +1,34 @@
-export function CrearCuenta(){
-  return <div>CrearCuenta</div>;
+import { useState } from "react";
+
+export function CrearCuenta() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = ({target: {name, value}}) => {
+    setUser({...user, [name]: value})
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        name="email"
+        placeholder="youremail@company.com"
+        onChange={handleChange}
+      />
+
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        onChange={handleChange}
+      />
+
+      <button>Register</button>
+    </form>
+  );
 }
