95f4190c6b524c2c65619695bf44d14c19ece4c8
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-25 18:30:41 -0300

    Borrando todo de IniciarSesion

--- src/components/IniciarSesion/IniciarSesion.jsx
+++ src/components/IniciarSesion/IniciarSesion.jsx
@@ -1,12 +1,5 @@
 import { useState } from "react";
 
 export function IniciarSesion(){
-  return <div>
-    <form>
-
-      <input type="email" name="email" id="email" />
-      <input type="password" name="password" id="password" />
-
-    </form>
-  </div>;
+	return <div>IniciarSesion</div>
 }
