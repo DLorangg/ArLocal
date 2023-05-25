4f34b136e5ed796abf29d31df318695079500dcd
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-25 16:03:02 -0300

    Importando provider

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import { Bienvenida } from './components/Bienvenida/Bienvenida';
 import { IniciarSesion } from './components/IniciarSesion/IniciarSesion';
 import { CrearCuenta } from './components/CrearCuenta/CrearCuenta';
-import { AuthProvider } from './context/authContext';
+import {AuthProvider} from './context/authContext';
 
 function App() {
   return(
     <div className='bg-slate-300 h-screen text-white flex'>
+      <AuthProvider>
       <Routes>
         <Route path="/" element={<Bienvenida/>} />
         <Route path="/login" element={<IniciarSesion/>} />
         <Route path="/register" element={<CrearCuenta/>} />
       </Routes>
+      </AuthProvider>
     </div>
   )
 }
