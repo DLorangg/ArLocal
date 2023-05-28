cec315397e37305962b61ee00419ef1ebdddab46
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-28 17:11:30 -0300

    Importando firebase-config

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext } from "react";
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from 'firebase';
+import { auth } from '../firebase-config';
 
 export const authContext = createContext();
 
