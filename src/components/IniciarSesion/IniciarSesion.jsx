31f9c8c96c610b6978544fac4ec8ec7f27d29905
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-28 20:14:14 -0300

    Pegando lo de CrearCuenta

--- src/components/IniciarSesion/IniciarSesion.jsx
+++ src/components/IniciarSesion/IniciarSesion.jsx
@@ -1,5 +1,60 @@
 import { useState } from "react";
+import { useAuth } from "../../context/authContext";
+import { useNavigate } from "react-router-dom";
 
-export function IniciarSesion(){
-	return <div>IniciarSesion</div>
+export function CrearCuenta() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const { signup } = useAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState();
+
+  const handleChange = ({ target: { name, value } }) =>
+    setUser({ ...user, [name]: value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await signup(user.email, user.password);
+      navigate("/");
+    } catch (error) {
+      console.log(error.code);
+      // Errores personalizados
+      if (error.code === "auth/internal-error") {
+        setError("Correo invalido");
+      }
+      if (error.code === "auth/weak-password") {
+        setError("La contraseña debe tener mínimo 6 caracteres");
+      }
+    }
+  };
+
+  return (
+    <div>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="youremail@company.com"
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="******"
+          onChange={handleChange}
+        />
+
+        <button>Register</button>
+      </form>
+    </div>
+  );
 }
