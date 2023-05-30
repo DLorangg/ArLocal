fab24b8e1197417dc9ba4071f9f87107f9850b78
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-30 15:50:18 -0300

    Usando propiedades del usuario loggeado

--- src/components/Bienvenida/Bienvenida.jsx
+++ src/components/Bienvenida/Bienvenida.jsx
@@ -1,9 +1,11 @@
-import { useAuth } from "../../context/authContext"
+import {useAuth} from '../../context/authContext'
 
 export function Bienvenida () {
 
   const {user} = useAuth()
   console.log(user);
 
-  return <div>Bienvenida</div>;
+  return <div>
+    <h1>Bienvenido {user.email}</h1>
+  </div>;
 }
