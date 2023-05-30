0c6095675471e61125e2e4e820d685cef2f132da
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-30 15:53:30 -0300

    Formateando codigo y creando la opcion para cerrar sesion

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,36 +1,39 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth';
-import { auth } from '../firebase-config';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../firebase-config";
 
 export const authContext = createContext();
 
 export const useAuth = () => {
-    const context = useContext(authContext);
-    return context;
+  const context = useContext(authContext);
+  return context;
 };
 
 export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-    const signup = (email, password) => 
-     createUserWithEmailAndPassword(auth, email, password);
+  const signup = (email, password) =>
+    createUserWithEmailAndPassword(auth, email, password);
 
-    const login = async (email, password) => 
-     await signInWithEmailAndPassword(auth, email, password);
-     
-    const logout = () => signOut(auth)
+  const login = async (email, password) =>
+    await signInWithEmailAndPassword(auth, email, password);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, currentUser =>{
-            setUser(currentUser);
-        })
-    }, [])
-    
+  const logout = () => signOut(auth);
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
 
-    return (
-        <authContext.Provider value={{ signup, login, user, logout }}>
-            {children}
-        </authContext.Provider>
-    );
+  return (
+    <authContext.Provider value={{ signup, login, user, logout }}>
+      {children}
+    </authContext.Provider>
+  );
 }
