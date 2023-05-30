9b04f4e924dfcce8c4e6dc6e51ca2155ab6a7f65
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-30 15:52:45 -0300

    Usando useState para ver el usuario loggeado

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase-config';
 
@@ -10,6 +10,8 @@ export const useAuth = () => {
 };
 
 export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+
     const signup = (email, password) => 
      createUserWithEmailAndPassword(auth, email, password);
 
@@ -19,14 +21,14 @@ export function AuthProvider({ children }) {
 
     useEffect(() => {
         onAuthStateChanged(auth, currentUser =>{
-            console.log(currentUser);
+            setUser(currentUser);
         })
     }, [])
     
 
 
     return (
-        <authContext.Provider value={{ signup, login }}>
+        <authContext.Provider value={{ signup, login, user}}>
             {children}
         </authContext.Provider>
     );
