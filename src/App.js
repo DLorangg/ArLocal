b9dd82780f3681b786e8ef931eabedb68856fa1f
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-30 16:17:07 -0300

    Formateando y agregando la ruta protegida

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
-import { Bienvenida } from './components/Bienvenida/Bienvenida';
-import { IniciarSesion } from './components/IniciarSesion/IniciarSesion';
-import { CrearCuenta } from './components/CrearCuenta/CrearCuenta';
-import {AuthProvider} from './context/authContext';
+import { Routes, Route } from "react-router-dom";
+import { Bienvenida } from "./components/Bienvenida/Bienvenida";
+import { IniciarSesion } from "./components/IniciarSesion/IniciarSesion";
+import { CrearCuenta } from "./components/CrearCuenta/CrearCuenta";
+import { AuthProvider } from "./context/authContext";
+import { RutaProtegida } from "./components/RutaProtegida/RutaProtegida";
 
 function App() {
-  return(
-      <AuthProvider>
+  return (
+    <AuthProvider>
       <Routes>
-        <Route path="/" element={<Bienvenida/>} />
-        <Route path="/login" element={<IniciarSesion/>} />
-        <Route path="/register" element={<CrearCuenta/>} />
+        <Route
+          path="/"
+          element={
+            <RutaProtegida>
+              <Bienvenida />
+            </RutaProtegida>
+          }
+        />
+        <Route path="/login" element={<IniciarSesion />} />
+        <Route path="/register" element={<CrearCuenta />} />
       </Routes>
-      </AuthProvider>
-  )
+    </AuthProvider>
+  );
 }
 
 export default App;
