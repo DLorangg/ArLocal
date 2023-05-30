51d7d5c98215ae9cf1ed7abfba89c6acfd7cc447
DLorangg <lorang.damian.epet20@gmail.com>
2023-05-30 15:57:03 -0300

    Creando y dandole funcion al boton para cerrar sesion

--- src/components/Bienvenida/Bienvenida.jsx
+++ src/components/Bienvenida/Bienvenida.jsx
@@ -1,11 +1,21 @@
+import { useNavigate } from 'react-router-dom'
 import {useAuth} from '../../context/authContext'
 
 export function Bienvenida () {
 
-  const {user} = useAuth()
-  console.log(user);
+  const {user, logout} = useAuth()
+   const navigate = useNavigate()
+  
+  const handleLogout = async () => {
+    await logout();
+    navigate("/");
+  }
 
   return <div>
     <h1>Bienvenido {user.email}</h1>
+
+    <button onClick={handleLogout}>
+      Cerrar sesión
+    </button>
   </div>;
 }
