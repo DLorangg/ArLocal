cfd58e2d3459c125c3a8fb4ebe3f4fd12615deb6
MateoFlores26 <floresmateojoaquin04@gmail.com>
2023-06-01 15:05:38 -0300

    Creando CrearCuenta

--- src/components/CrearCuenta/CrearCuenta.jsx
+++ src/components/CrearCuenta/CrearCuenta.jsx
@@ -1,8 +1,89 @@
 import React from 'react'
 
-export const CrearCuenta = () => {
+export function CrearCuenta () {
   return (
-    <div>CrearCuenta</div>
-    //
+    <div>
+      <section className="flex flex-col md:flex-row h-screen items-center">
+
+        <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+          <img src="https://source.unsplash.com/random" alt="" className="w-full h-full object-cover"/>
+        </div>
+
+        <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12  flex items-center justify-center">
+
+          <div className="w-full h-100">
+
+
+            <h1 className="text-xl md:text-2xl font-bold leading-tight mt-12">Crear una Cuenta</h1>
+
+            <form className="mt-6" action="#" method="POST">
+              <div>
+                <label className="block text-gray-700">Correo Electrónico</label>
+                  <input type="email" name="" id="" placeholder="Ingresa tu Correo Electrónico" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required/>
+              </div>
+
+              <div className="mt-4">
+                <label className="block text-gray-700">Contraseña</label>
+                <input type="password" name="" id="" placeholder="Ingresa tu Contraseña" minlength="6" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required/>
+              </div>
+
+              <div className="mt-4">
+                <label className="block text-gray-700">Confirmar Contraseña</label>
+                <input type="password" name="" id="" placeholder="Ingresa tu Contraseña" minlength="6" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required/>
+              </div>
+
+              <button type="submit" className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">Crear Cuenta</button>
+            </form>
+
+            <hr className="my-6 border-gray-300 w-full"/>
+
+            <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+              <div className="flex items-center justify-center">
+                  <svg class="w-4" viewBox="0 0 533.5 544.3">
+                    <path
+                      d="M533.5 278.4c0-18.5-1.5-37.1-4.7-55.3H272.1v104.8h147c-6.1 33.8-25.7 63.7-54.4 82.7v68h87.7c51.5-47.4 81.1-117.4 81.1-200.2z"
+                      fill="#4285f4"
+                    />
+                    <path
+                      d="M272.1 544.3c73.4 0 135.3-24.1 180.4-65.7l-87.7-68c-24.4 16.6-55.9 26-92.6 26-71 0-131.2-47.9-152.8-112.3H28.9v70.1c46.2 91.9 140.3 149.9 243.2 149.9z"
+                      fill="#34a853"
+                    />
+                    <path
+                      d="M119.3 324.3c-11.4-33.8-11.4-70.4 0-104.2V150H28.9c-38.6 76.9-38.6 167.5 0 244.4l90.4-70.1z"
+                      fill="#fbbc04"
+                    />
+                    <path
+                      d="M272.1 107.7c38.8-.6 76.3 14 104.4 40.8l77.7-77.7C405 24.6 339.7-.8 272.1 0 169.2 0 75.1 58 28.9 150l90.4 70.1c21.5-64.5 81.8-112.4 152.8-112.4z"
+                      fill="#ea4335"
+                    />
+                  </svg>
+                <span className="ml-4">Crear con Google</span>
+              </div>
+            </button>
+
+            <br/>
+            
+            <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+              <div className="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="#00acee" class="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg>
+                <span className="ml-3">Crear con Twitter</span>
+              </div>
+            </button>
+
+            <br/>
+
+            <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+              <div className="flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#3b5998" class="bi bi-facebook" viewBox="0 0 16 16"> <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/> </svg>
+                <span className="ml-3">Crear con Facebook</span>
+              </div>
+            </button>
+
+            <p className="mt-8">¿Ya tienes una Cuenta? <a href="#" className="text-blue-500 hover:text-blue-700 font-semibold">Ingresar</a></p>
+
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
