8df7fe8b5899a4cec3257a45b667caea73aa7582
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-06-20 20:17:40 -0300

    Creo estilos para los items

--- src/components/LocalesItem.jsx
+++ src/components/LocalesItem.jsx
@@ -1,9 +1,20 @@
 import react from "react";
 import {Text, View, StyleSheet} from 'react-native';
-
+const styles = StyleSheet.create({
+    contenedor: {
+        padding:20,
+        paddingBottom:5,
+        paddingTop:5
+    },
+    titulo:{
+        fontWeight:'bold', 
+        marginBottom:5,
+        color:'#0945'
+    }
+})
 const LocalesItem=(props)=>(
-    <View key={props.id} style={{padding:20, paddingBottom:5, paddingTop:5}}>
-    <Text style={{fontWeight:'bold', marginBottom:5}}>{props.nombre}</Text>
+    <View key={props.id} style={styles.contenedor}>
+    <Text style={styles.titulo}>{props.nombre}</Text>
     <Text>@{props.id}</Text>
     <Text>{props.categoria}</Text>
     <Text>{props.direccion}!</Text>
