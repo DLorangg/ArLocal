d72a6d6cb7f3a341002ca28fd589888e9f1c7e2a
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-06-20 20:09:52 -0300

    Creo componente LocalesItem para LocalesList

---
+++ src/components/LocalesItem.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+
+const LocalesItem=(props)=>(
+    <View key={props.id} style={{padding:20, paddingBottom:5, paddingTop:5}}>
+    <Text style={{fontWeight:'bold', marginBottom:5}}>{props.nombre}</Text>
+    <Text>@{props.id}</Text>
+    <Text>{props.categoria}</Text>
+    <Text>{props.direccion}!</Text>
+    {/* <Image
+    source={local.img}/> */}
+    
+    
+</View>
+)
+export default LocalesItem
--- src/components/LocalesList.jsx
+++ src/components/LocalesList.jsx
@@ -1,6 +1,7 @@
 import react from "react";
 import { View, Text, Image, FlatList } from "react-native";
 import locales from "./data/locales";
+import LocalesItem from "./LocalesItem";
 
 const LocalesList = () =>{
     return(
@@ -8,16 +9,8 @@ const LocalesList = () =>{
         data={locales}
         ItemSeparatorComponent={()=><Text></Text>}
         renderItem={({item: local})=>(
-            <View key={local.id} style={{padding:20, paddingBottom:5, paddingTop:5}}>
-                    <Text style={{fontWeight:'bold', marginBottom:5}}>{local.nombre}</Text>
-                    <Text>@{local.id}</Text>
-                    <Text>{local.categoria}</Text>
-                    <Text>{local.direccion}</Text>
-                    {/* <Image
-                    source={local.img}/> */}
-                    
-                    
-                </View>
+            <LocalesItem {... local}/>
+
         )}/>
     )
 }
