eb969d7b374fe9ca377bf81a0d7ed133fa4e6066
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-06-20 21:39:26 -0300

    Cambios en los items

--- src/components/LocalesItem.jsx
+++ src/components/LocalesItem.jsx
@@ -1,5 +1,5 @@
 import react from "react";
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet,Image} from 'react-native';
 const styles = StyleSheet.create({
     contenedor: {
         padding:20,
@@ -10,14 +10,29 @@ const styles = StyleSheet.create({
         fontWeight:'bold', 
         marginBottom:5,
         color:'#0945'
+    },
+    categoria: {
+        backgroundColor:'#0945',
+        padding: 4,
+        alignSelf:'flex-start',
+        borderRadius:4
+
+    },
+    imagen:{
+        width:150,
+        height:150,
+        borderRadius:4
     }
 })
 const LocalesItem=(props)=>(
     <View key={props.id} style={styles.contenedor}>
+    <Image style={styles.imagen} source={{uri: props.img}}/>
     <Text style={styles.titulo}>{props.nombre}</Text>
     <Text>@{props.id}</Text>
-    <Text>{props.categoria}</Text>
-    <Text>{props.direccion}!</Text>
+    <View style={styles.categoria}>
+    <Text >{props.categoria}</Text>
+    </View>
+    <Text>{props.direccion}</Text>
     {/* <Image
     source={local.img}/> */}
     
