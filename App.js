5deb8bb6c8544cf731286723f26254faabc31e10
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-06-21 13:53:42 -0300

    Creo componente para el header

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 import Navigation from "./Navigation";
+import AppBar from "./src/components/AppBar";
+
 const App =()=>{
 
   return(
-
-
+    <>
+      <AppBar/>
       <Navigation/>
+
+    </>
+
 
   ) 
 };
---
+++ src/components/AppBar.jsx
@@ -0,0 +1,38 @@
+import react from "react";
+import {ViewPropTypes, StyleSheet, Text, Image} from 'react-native'
+import Constants from 'expo-constants'
+import { View } from "react-native";
+const styles = StyleSheet.create({
+    contenedor:{
+        backgroundColor:'#5C4999',
+        paddingTop: Constants.statusBarHeight +30,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        flexDirection: 'row'
+
+    },
+    img:{
+        width:45,
+        height:45,
+
+
+    }
+})
+
+const AppBar = ()=>{
+    return(
+        <View style={styles.contenedor}>
+            <Image
+            style={styles.img}
+            source={require('../components/../../assets/logos/7.png')}/>
+            <Text style={styles.tex}>
+                ArLocal
+            </Text>
+
+
+        </View>
+
+    )
+}
+export default AppBar
+// background: #5C4999;
