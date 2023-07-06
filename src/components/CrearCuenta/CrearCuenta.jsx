5dc4cc6c2e68719c88707c4bdd0818b7fc5d2281
DLorangg <lorang.damian.epet20@gmail.com>
2023-07-06 15:41:09 -0300

    Metiendo funcionalidad al formulario

--- src/components/CrearCuenta/CrearCuenta.jsx
+++ src/components/CrearCuenta/CrearCuenta.jsx
@@ -50,18 +50,13 @@ export function CrearCuenta() {
           {error && <p>{error}</p>}
           <form className="mt-6" onSubmit={handleSubmit}>
             <div>
-              <label className="block text-gray-700">Correo Electrónico</label>
-                <input type="email" name="" id="" placeholder="Ingresa Correo Electrónico" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required/>
+              <label htmlFor="email" className="block text-gray-700">Correo Electrónico</label>
+                <input type="email" name="email" onChange={handleChange} placeholder="Ingresa Correo Electrónico" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required/>
             </div>
 
             <div className="mt-4">
-              <label className="block text-gray-700">Contraseña</label>
-              <input type="password" name="" id="" placeholder="Ingresa Contraseña" minlength="6" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required/>
-            </div>
-
-            <div className="mt-4">
-              <label className="block text-gray-700">Confirmar Contraseña</label>
-              <input type="password" name="" id="" placeholder="Confirmar Contraseña" minlength="6" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required/>
+              <label htmlFor="password" className="block text-gray-700">Contraseña</label>
+              <input type="password" name="password" onChange={handleChange} placeholder="Ingresa Contraseña" minlength="8" className="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" required/>
             </div>
 
             <button type="submit" className="w-full block bg-indigo-500 hover:bg-indigo-400 focus:bg-indigo-400 text-white font-semibold rounded-lg px-4 py-3 mt-6">Crear Cuenta</button>
@@ -69,7 +64,7 @@ export function CrearCuenta() {
 
           <hr className="my-6 border-gray-300 w-full"/>
 
-          <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+          <button type="button" onClick={handleGoogleSignIn} className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
             <div className="flex items-center justify-center">
                 <svg class="w-4" viewBox="0 0 533.5 544.3">
                   <path
@@ -97,7 +92,7 @@ export function CrearCuenta() {
           
           <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
             <div className="flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="#00acee" class="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="#00acee" classname="bi bi-twitter" viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg>
               <span className="ml-3">Ingresar con Twitter</span>
             </div>
           </button>
@@ -106,12 +101,12 @@ export function CrearCuenta() {
 
           <button type="button" className="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
             <div className="flex items-center justify-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#3b5998" class="bi bi-facebook" viewBox="0 0 16 16"> <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/> </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="#3b5998" classname="bi bi-facebook" viewBox="0 0 16 16"> <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/> </svg>
               <span className="ml-3">Ingresar con Facebook</span>
             </div>
           </button>
 
-          <p className="mt-8">¿Ya tienes una Cuenta? <a href="#" className="text-blue-500 hover:text-blue-700 font-semibold">Ingresar</a></p>
+          <p className="mt-8">¿Ya tienes una Cuenta? <a href="login" className="text-blue-500 hover:text-blue-700 font-semibold">Ingresar</a></p>
 
         </div>
       </div>
@@ -121,21 +116,13 @@ export function CrearCuenta() {
 }
 
 /*
-  <div>
-        <input
-          type="email"
-          name="email"
-          placeholder="youremail@company.com"
-          onChange={handleChange}
-        />
-
         <label htmlFor="password">Password</label>
         <input
           type="password"
           name="password"
           id="password"
           placeholder="******"
-          onChange={handleChange}
+          
         />
 
         <button>Register</button>
