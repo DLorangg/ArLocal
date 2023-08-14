988aca5cc900dfeda8a60c9036f955c264890859
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-08-14 15:34:49 -0300

    Agrego text input

--- src/components/BarraBusqueda.jsx
+++ src/components/BarraBusqueda.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import {SafeAreaView, StyleSheet, TextInput, View, Text} from 'react-native';
 
 const BarraBusqueda = () =>{
+    const [text, onChangeText] = React.useState('Useless Text');
+
     return (
-        <View>
-            <TextInput/>
-        </View>
+        <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+
+      </SafeAreaView>
     )
 
 
