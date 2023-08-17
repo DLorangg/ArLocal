767fb3d83ab2a4c120f6d444dc8e3c12ef1d1d39
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-08-17 16:39:45 -0300

    Prop del padre

--- src/components/BarraBusqueda.jsx
+++ src/components/BarraBusqueda.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {SafeAreaView, StyleSheet, TextInput, View, Text} from 'react-native';
 
-const BarraBusqueda = () =>{
+const BarraBusqueda = ({ actualizarTextoBusqueda }) =>{
   const [texto, setTexto] = React.useState('');
   
     return (
