95b213f41cc4fe6e0b79daef594acb08ebf5926e
DLorangg <lorang.damian.epet20@gmail.com>
2023-08-20 22:54:14 -0300

    Creando todo el backend para el Login

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,35 +1,101 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import React, { useState } from 'react'
-import { auth } from '../../Firebase';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+  Button,
+  KeyboardAvoidingView,
+} from "react-native";
+import React, { useState } from "react";
+import { auth } from "../../Firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      console.log(response);
+    } catch (error: any) {
+      console.log(error);
+      alert("Problema al iniciar sesion: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(response);
+      alert("Fijate tu mail!");
+    } catch (error: any) {
+      console.log(error);
+      alert("Problema al registrarse: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <TextInput value={email} style={styles.input} placeholder='Email' autoCapitalize='none' onChangeText={(text => setEmail(text))}></TextInput>
-      <TextInput secureTextEntry={true} value={password} style={styles.input} placeholder='Password' autoCapitalize='none' onChangeText={(text => setPassword(text))}></TextInput>
+        <KeyboardAvoidingView behavior="padding">
+      <TextInput
+        value={email}
+        style={styles.input}
+        placeholder="Email"
+        autoCapitalize="none"
+        onChangeText={(text) => setEmail(text)}
+      ></TextInput>
+      <TextInput
+        secureTextEntry={true}
+        value={password}
+        style={styles.input}
+        placeholder="Password"
+        autoCapitalize="none"
+        onChangeText={(text) => setPassword(text)}
+      ></TextInput>
+
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <>
+          <Button title="Iniciar Sesión" onPress={signIn} />
+          <Button title="Registrarse" onPress={signUp} />
+        </>
+      )}
+      </KeyboardAvoidingView>
     </View>
   );
 };
 
-export default Login
+export default Login;
 
 const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 20,
-        flex: 1,
-        justifyContent: 'center'
-    },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: '#fff'
-    }
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
 });
-
