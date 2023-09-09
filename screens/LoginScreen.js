c7df6667824bb456bba8e375affc224898ed51aa
DLorangg <lorang.damian.epet20@gmail.com>
2023-09-09 18:56:57 -0300

    Creando pantalla de Login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,64 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+import RegistrarseScreen from "./RegistrarseScreen";
 
 const LoginScreen = () => {
+  const navigation = useNavigation(); // Obtiene la instancia de navegación
+
   return (
-    <View>
-      <Text>Login Screen</Text>
-    </View>
-  )
-}
+    <KeyboardAvoidingView
+      styles={styles.container}
+      behavior="padding"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => { }}
+          style={styles.button}
+        >
+          <Text style={styles.button}>Login</Text>
+        </TouchableOpacity>
+        <Text>
+          No tienes una cuenta?{' '}
+          <TouchableOpacity onPress={() => navigation.navigate('Registrarse')}>
+            <Text style={styles.registerLink}>Regístrate!</Text>
+          </TouchableOpacity>
+        </Text>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
 export default LoginScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 40,  
+  },
+  
+})
