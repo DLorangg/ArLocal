a0d212ed22ed886448af84605a82ad5b0863b80b
DLorangg <lorang.damian.epet20@gmail.com>
2023-09-12 20:28:49 -0300

    Haciendo funcion para registrarse

--- screens/RegistrarseScreen.js
+++ screens/RegistrarseScreen.js
@@ -1,11 +1,23 @@
 import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
-import LoginScreen from "./LoginScreen";
+import { auth } from '../Firebase'
 
 const RegistrarseScreen = () => {
     const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSignUp = () => {
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then(userCredentials => {
+          const user = userCredentials.user;
+          console.log('Registrado con:', user.email);
+        })
+        .catch(error => alert(error.message))
+    }
     
     return (
       <KeyboardAvoidingView
@@ -15,14 +27,14 @@ const RegistrarseScreen = () => {
         <View style={styles.inputContainer}> 
           <TextInput
             placeholder="Email"
-            //value={ }
-            //onChangeText={text => }
+            value={email}
+            onChangeText={text => setEmail(text)}
             style={styles.input}
           />
           <TextInput
             placeholder="Password "
-            //value={ }
-            //onChangeText={text => }
+            value={password}
+            onChangeText={text => setPassword(text)}
             style={styles.input}
             secureTextEntry
           />
@@ -30,7 +42,7 @@ const RegistrarseScreen = () => {
   
         <View style={styles.buttonContainer}>
           <TouchableOpacity
-            onPress={() => { }}
+            onPress={handleSignUp}
             style={styles.button}
           >
             <Text style={styles.button}>Registrarse</Text>
@@ -48,4 +60,37 @@ const RegistrarseScreen = () => {
 
 export default RegistrarseScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,  
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '40%',
+  },
+  button: {
+    backgroundColor: '#6ee674',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  }
+})
