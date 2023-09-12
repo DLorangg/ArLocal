7254f3ba473ad5ffda7f66b7c2dce3a8218efa36
DLorangg <lorang.damian.epet20@gmail.com>
2023-09-12 20:29:16 -0300

    Creando funcion para cerrar sesión y mostrando email loggeado

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { auth } from '../Firebase'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+  const navigation = useNavigation()
+  
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login")
+      })
+      .catch(error => alert(error.message))
+  }
+
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+      >
+        <Text>Cerrar sesión</Text>
+      </TouchableOpacity>
     </View>
   )
 }
