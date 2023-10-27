ad2e0de56a9744fd8e5f8abca7ecb5f1f50f06a7
DLorangg <lorang.damian.epet20@gmail.com>
2023-10-26 21:55:53 -0300

    Agregando asyncStorage

--- Firebase.ts
+++ Firebase.ts
@@ -1,18 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore';
 
-//  My app's Firebase configuration   
 const firebaseConfig = {
   apiKey: "AIzaSyCRu60fNVqAZrLXio9buUSN768MT8uCO7U",
   authDomain: "arlocal.firebaseapp.com",
   projectId: "arlocal",
   storageBucket: "arlocal.appspot.com",
   messagingSenderId: "297451769578",
-  appId: "1:297451769578:web:4306bf0795b430c7e6d193"
+  appId: "1:297451769578:web:4306bf0795b430c7e6d193",
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+}); export {auth}
 export const firestore = getFirestore(app);
