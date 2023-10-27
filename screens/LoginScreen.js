a047191248fcd2b79e57d7539043c15d8779d04d
DLorangg <lorang.damian.epet20@gmail.com>
2023-10-26 21:56:07 -0300

    Agregando para mandar mail de cambio de contraseña

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,7 +2,7 @@ import { KeyboardAvoidingView, StyleSheet, Text, TextInput, View, TouchableOpaci
 import React, { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { auth } from '../Firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 
 const LoginScreen = () => {
   const navigation = useNavigation(); // Obtiene la instancia de navegación
@@ -20,14 +20,45 @@ const LoginScreen = () => {
   }, [])
 
   const handleLogin = () => {
-      signInWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredentials) => {
         const user = userCredentials.user;
         console.log('Inicio sesión con:', user.email);
-        navigation.navigate('Tabs')
+        navigation.navigate('Tabs');
       })
-      .catch(error => alert(error.message))
-  }
+      .catch((error) => {
+        switch (error.code) {
+          case 'auth/invalid-email':
+            alert('Email inválido. Debe contener "@".');
+            break;
+          case 'auth/wrong-password':
+            alert('Contraseña incorrecta.');
+            break;
+          case 'auth/user-not-found':
+            alert('Usuario no encontrado. Regístrate primero.');
+            break;
+          case 'auth/too-many-requests':
+            alert('Demasiados intentos fallidos. La cuenta se ha bloqueado, cambia la contraseña.');
+            break;
+          default:
+            alert('Error desconocido: ' + error.message);
+        }
+      });
+  }  
+  
+  const handleChangePassword = () => {
+    if (!email) {
+      alert('Por favor, ingrese su dirección de correo electrónico antes de solicitar el restablecimiento de contraseña.');
+    } else {
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          alert('Se ha enviado un correo para restablecer la contraseña. Revise su bandeja de entrada.');
+        })
+        .catch((error) => {
+          alert('Error al enviar el correo para restablecer la contraseña: ' + error.message);
+        });
+    }
+  };
 
   return (
 
@@ -60,6 +91,12 @@ const LoginScreen = () => {
         >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleChangePassword}
+          style={styles.changePasswordButton}
+        >
+          <Text style={styles.changePasswordButtonText}>Cambiar contraseña</Text>
+        </TouchableOpacity>
         <Text>
           No tienes una cuenta?{' '}
           <TouchableOpacity onPress={() => navigation.navigate('Registrarse')}>
@@ -76,22 +113,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
   inputContainer: {
-    width: '80%'
+    width: '80%',
+    marginBottom: 20,
   },
   input: {
     backgroundColor: 'white',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
-    marginTop: 5,  
+    marginTop: 5,
   },
   buttonContainer: {
     width: '60%',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: '40%',
+    marginTop: 20,
   },
   button: {
     backgroundColor: '#6ee674',
@@ -103,7 +142,13 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: '700',
     fontSize: 16,
-  }
-})
+    textAlign: 'center',
+  },
+  changePasswordLink: {
+    color: 'blue',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+});
 
 export default LoginScreen
