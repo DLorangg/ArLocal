9114104536577fe26b5ed79adebdfb705e561117
DLorangg <lorang.damian.epet20@gmail.com>
2023-10-27 01:26:05 -0300

    Quitando espacios innecesarios

--- src/components/BarraBusqueda.jsx
+++ src/components/BarraBusqueda.jsx
@@ -1,39 +1,50 @@
 import React from "react";
-import {SafeAreaView, StyleSheet, TextInput, View, Text} from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, View, Text } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const BarraBusqueda = ({ actualizarTextoBusqueda }) =>{
+const BarraBusqueda = ({ actualizarTextoBusqueda }) => {
   const [texto, setTexto] = React.useState('');
 
-  const handleActualizarTexto = (nuevoTexto) => { //funcion para cuando se actualice el texto
-    setTexto(nuevoTexto); //cambia el estado del texto
-    actualizarTextoBusqueda(nuevoTexto); // Llama a la función de devolución de llamada del padre
+  const handleActualizarTexto = (nuevoTexto) => {
+    setTexto(nuevoTexto);
+    actualizarTextoBusqueda(nuevoTexto);
+  };
 
-  };
-  
-    return (
-      <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={handleActualizarTexto} // Usa la nueva función de cambio de texto
-        value={texto}
-      />
-      {/* <Text style={styles.text}>{texto}</Text> */}
+  return (
+    <SafeAreaView>
+      <View style={styles.inputContainer}>
+        <MaterialCommunityIcons name="magnify" size={24} color="#9C85E6" style={styles.icon} />
+        <TextInput
+          style={styles.input}
+          onChangeText={handleActualizarTexto}
+          value={texto}
+        />
+      </View>
     </SafeAreaView>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    input: {
-      height: 40,
-      margin: 12,
-      borderWidth: 1,
-      padding: 10,
-    },
-    text: {
-        margin: 12,
-        fontSize: 16,
-      },
-  });
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: '#9C85E6', // Establece el color del borde
+    flex: 1,
+  },
+  text: {
+    margin: 12,
+    fontSize: 16,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    paddingHorizontal: 5, // Agrega espacio en los lados
+  },
+  icon: {
+    marginLeft: 10,
+  }
+});
 
 export default BarraBusqueda;
-
