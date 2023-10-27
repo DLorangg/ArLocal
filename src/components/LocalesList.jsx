95c6d14180343f196d83490f91b4e48dba717742
DLorangg <lorang.damian.epet20@gmail.com>
2023-10-26 23:46:11 -0300

    Haciendo la conexion con firestore

--- src/components/LocalesList.jsx
+++ src/components/LocalesList.jsx
@@ -1,42 +1,37 @@
-import react from "react";
-import {Text, FlatList } from "react-native";
-import locales from "./data/locales";
-import LocalesItem from "./LocalesItem";
+import React, { useEffect, useState } from 'react';
+import { Text, FlatList } from 'react-native';
+import { collection, getDocs } from 'firebase/firestore';
+import { firestore } from '../../Firebase'; 
+import LocalesItem from './LocalesItem';
 
-const LocalesList = ({busqueda}) =>{
-// weas->
-const x= (locales, input) =>{ 
-    const coincidencia= ""
-    locales.forEach( (local)=> { 
-        if (input === local.nombre){
-            return local
+const LocalesList = ({ busqueda }) => {
+  const [locales, setLocales] = useState([]);
 
-        }
-    } )
+  useEffect(() => {
+    const fetchLocalesFromFirestore = async () => {
+      const localCollection = collection(firestore, 'local'); // Usar 'local' como nombre de la colección
+      const querySnapshot = await getDocs(localCollection);
+
+      const localesData = [];
+      querySnapshot.forEach((doc) => {
+        localesData.push(doc.data());
+      });
+
+      setLocales(localesData);
+    };
+
+    fetchLocalesFromFirestore();
+  }, []);
+
+  // Puedes agregar la lógica para filtrar locales basados en la búsqueda aquí
+
+  return (
+    <FlatList
+      data={locales}
+      ItemSeparatorComponent={() => <Text></Text>}
+      renderItem={({ item: local }) => <LocalesItem {...local} />}
+    />
+  );
 }
 
-// function filtrarNombre(productos) {
-//     const inputFiltrado = document.getElementById('inputFiltrado')
-//     let busqueda
-//     let rta;
-//     inputFiltrado.addEventListener('keyup', function() {
-//         busqueda = inputFiltrado.value;
-//         rta = productos.filter((producto) => producto.nombre.toLowerCase().includes(busqueda.toLowerCase()))
-//         listarProductos(rta, menu, true)
-//     })
-
-// }
-//<-weas
-    return(
-         <FlatList
-         data={locales}
-         ItemSeparatorComponent={()=><Text></Text>}
-         renderItem={({item: local})=>(
-             <LocalesItem {... local}/>
-
-         )}/>
-//<Text>{busqueda}</Text>
-    )
-}
-
-export default LocalesList
+export default LocalesList;
