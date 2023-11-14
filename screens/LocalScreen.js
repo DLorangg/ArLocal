7270733effd994942a55622196ac92627adfa059
MateoFlores26 <floresmateojoaquin04@gmail.com>
2023-11-14 11:15:45 -0300

    Creando screen LocalScreen

---
+++ screens/LocalScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native'
+
+const LocalScreen = () => {
+
+
+  return (
+    <View>
+        <Text>Local</Text>
+    </View>
+  );
+};
+
+export default LocalScreen;
